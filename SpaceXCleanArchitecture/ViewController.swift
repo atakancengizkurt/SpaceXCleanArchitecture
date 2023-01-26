9218f0dde4f8cca27bf69196519b10c8cd718c53
Atakan Cengiz KURT <atakan1903@hotmail.com>
2023-01-26 21:11:06 +0300

    Initial Commit

---
+++ SpaceXCleanArchitecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceXCleanArchitecture
+//
+//  Created by Atakan Cengiz KURT on 26.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
